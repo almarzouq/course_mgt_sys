2aed486f73ad932f9b6b36cf086642c515a55b28
Emperor <nsoulper@gmail.com>
2015-11-21 09:38:23 +0300

    second commit

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import include, url
 from students import views
 
 urlpatterns = [
-url(r'^register/$',views.StudentRegister.as_view(),name="student_register",),
-url(r'^edit/$'views.edit_profile , name="student_profile_edit")
+url(r'^edit/',views.edit_profile , name="student_profile_edit",),
 ]
--- students/views.py
+++ students/views.py
@@ -4,23 +4,15 @@ from django.views.generic.edit import CreateView , UpdateView
 from forms import StudentEdit
 # Create your views here.
 
-class StudentRegister(CreateView):
-    #to use the models variable
-    model = Student
-    #we require all fields since students will need register him/her self
-    fields = '__all__'
-    template_name = "Student_Profile_Create.html"
-    #after succeding in the registery proccess the student will be redirected to this directery
-    success_url = ("Home",)
 
 def edit_profile(request):
-    obj = request.user.student
+    obj = request.user
     if request.method == 'POST':
         form = StudentEdit(request.POST, instance=obj)
         if form.is_valid():
             form.save()
             return redirect('Home')
     else:
-        form = UserProfileForm(instance=obj)
+        form = StudentEdit(instance=obj)
 
-        return render(request, 'Student_Profile_Create.html', {'profile': obj, 'form': form, })
+        return render(request, 'Student_Profile_Edit.html', {'profile': obj, 'form': form, })
