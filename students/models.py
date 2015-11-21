84eee1dae5f868d871e3e7804911d21f860651e5
nalorakq8 <yousefmon@gmail.com>
2015-11-21 13:44:37 +0300

    testing atom beautify

--- students/models.py
+++ students/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Student(models.Model):
     name = models.CharField(max_length=120)
     university_id = models.BigIntegerField()
