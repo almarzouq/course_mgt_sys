9ed84b88f935e66b0cfe5fd4aea50868b08ac373
nalorakq8 <yousefmon@gmail.com>
2015-11-21 13:46:46 +0300

    used atom beautify to fix pep8 error

--- instructors/admin.py
+++ instructors/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Instructor
 
-class InstructorAdmin (admin. ModelAdmin ):
-	list_display = ('name' ,'email', 'phone' , 'office_hours' , 'department' , 'school' , 'twitter_id')
 
-admin.site.register(Instructor , InstructorAdmin)
+class InstructorAdmin (admin. ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'office_hours',
+                    'department', 'school', 'twitter_id')
+
+admin.site.register(Instructor, InstructorAdmin)
