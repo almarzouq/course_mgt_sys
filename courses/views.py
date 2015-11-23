a517fe9f8476f6064bee2e15bcec113e92861018
Monia Alobaid <moniatie-94@hotmail.com>
2015-11-23 12:06:39 +0300

    saving progress

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .forms import NewCourseForm
 # Create your views here.
+
+def course_create (request):
+    if request.method== 'POST':
+        form = NewCourseForm(request.POST)
+        if form.is_valid():
+            new_course = form.save(commit=False)
+    else:
+        form = NewCourseForm()
+    return render(request,
+    )
