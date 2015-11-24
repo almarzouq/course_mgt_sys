9e49856c12799ba8cb3d77c7a83d821aabe4c449
Lujain Al Subaie <lujainalsubaie@gmail.com>
2015-11-24 19:44:58 +0300

    Instructor can edit profile

--- instructors/views.py
+++ instructors/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import Instructor, Announcement
 
@@ -29,3 +29,10 @@ class InstructorRegister(CreateView):
     model = Instructor
     fields = '__all__'
     template_name = 'instructor_create_profile.html'
+
+
+class InstructorEditProfile(UpdateView):
+    model = Instructor
+    fields = ['phone', 'email', 'office_hours', 'twitter_id', ]
+    template_name = 'instructor_profile_edit.html'
+    context_object_name = "form"
