dc6fad5edce202117d4f37aef45d00b9af697c48
Emperor <nsoulper@gmail.com>
2015-11-25 22:35:42 +0300

    added template and CBV

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^myprofile/$', views.student_profile, name='student_profile'),
     url(r'^register/$', views.StudentRegister.as_view(),
         name="student_register",),
-    url(r'^edit/(?P<student_id>\d+)', views.edit_profile, name="student_edit",),
+    url(r'^edit/', views.edit_profile, name="student_edit",),
 
 ]
