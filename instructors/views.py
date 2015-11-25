5c61507fe1233c62455e0d88bae32f6137bcc671
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2015-11-25 07:32:34 +0300

    general cleanup

--- instructors/views.py
+++ instructors/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
-from .models import Instructor, Announcement
+from .models import Instructor
 
 
 # Create your views here.
