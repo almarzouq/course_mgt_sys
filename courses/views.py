ef87118d3fdcc7d82463accb55102cafae742123
Monia Alobaid <moniatie-94@hotmail.com>
2015-12-01 11:51:58 +0300

    removing unecessary changes

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from django.contrib import messages
+
 from .forms import NewCourseForm
+from .models import GradeColumn
+
 from students.models import Student
 from courses.models import Course
 # Create your views here.
@@ -23,16 +27,10 @@ def course_create(request):
     )
 
 
-def list_course_grade_column(request, course_id):
-    course_obj = get_object_or_404(Course, pk=course_id)
-    qs = course_obj.gradecolumn_set.all()
+class CourseGradeView(ListView):
+    model = GradeColumn
+    template_name = "course_grade.html"
 
-    return render(request, "course_grade.html",
-    {
-        'course': course_obj,
-        'gradecolumns': qs,
-    }
-    )
 
 def enroll_student_to_course(request, course_id, student_id):
     course = Course.objects.get(pk=course_id)
