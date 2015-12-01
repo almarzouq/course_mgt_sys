de8fcd7c9cf42ca38e38c8d909e52da16584db57
nalorakq8 <yousefmon@gmail.com>
2015-12-01 15:20:27 +0300

    fixed url

--- courses/urls.py
+++ courses/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
         name='course_grade'),
     url(r'^course/(?P<course_id>\d+)/enroll/(?P<student_id>\d+)',
         views.enroll_student_to_course, name='enroll'),
-    url(r'^course/post/$', views.post_student_grade,
+    url(r'^course/post/grade$', views.post_student_grade,
         name='post_student_grade'),
 
 ]
