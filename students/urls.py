7c13c522fac918b422c4fe6a081b414901cf1142
Emperor <nsoulper@gmail.com>
2015-12-01 22:22:54 +0300

    voided urls problem

--- students/urls.py
+++ students/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^register/$', views.StudentRegister.as_view(),
         name="student_register",),
     url(r'^student/edit/', views.edit_profile, name="student_edit",),
-    url(r'^student/list', views.students_list, name='students_list',),
+    url(r'^student/list$', views.students_list, name='students_list',),
     url(r'^student/list/uni/$', ListView.as_view(template_name='student_list.html',
                                                  context_object_name='students',
                                                  queryset=Student.objects.order_by('university_id'),), name='student_list_order_uni'),
