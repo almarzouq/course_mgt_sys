bae1c168ca17e64269b90b0a5e7270a87c8add65
Monia Alobaid <moniatie-94@hotmail.com>
2015-12-01 11:34:24 +0300

    set students and syllabusurl to null in course model

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-
 from .forms import NewCourseForm
-from .models import GradeColumn
-
 from students.models import Student
 from courses.models import Course
 # Create your views here.
@@ -27,10 +23,16 @@ def course_create(request):
     )
 
 
-class CourseGradeView(ListView):
-    model = GradeColumn
-    template_name = "course_grade.html"
+def list_course_grade_column(request, course_id):
+    course_obj = get_object_or_404(Course, pk=course_id)
+    qs = course_obj.gradecolumn_set.all()
 
+    return render(request, "course_grade.html",
+    {
+        'course': course_obj,
+        'gradecolumns': qs,
+    }
+    )
 
 def enroll_student_to_course(request, course_id, student_id):
     course = Course.objects.get(pk=course_id)
