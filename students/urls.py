59e29fd9e9b2a8d5e817adae598a9ffa2b9708bb
Emperor <nsoulper@gmail.com>
2015-12-01 22:17:23 +0300

    void

--- students/urls.py
+++ students/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from students import views
 
-
 urlpatterns = [
     url(r'^student/(?P<pk>\d+)$', views.student_profile, name='student_view',),
     url(r'^register/$', views.StudentRegister.as_view(),
         name="student_register",),
     url(r'^student/edit/', views.edit_profile, name="student_edit",),
-    url(r'^student/list', views.students_list, name='students_list',),
+    url(r'^student/list$', views.students_list, name='students_list',),
+
 
 ]
