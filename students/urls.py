258f24f240a6ee1bd2e872398d890834126a7c39
Emperor <nsoulper@gmail.com>
2015-12-01 08:17:45 +0300

    fixed issue

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from students import views
 
 
 urlpatterns = [
-    url(r'^profile/student/(?P<pk>\d+)$', views.student_profile, name='student_view',),
+    url(r'^student/(?P<pk>\d+)$', views.student_profile, name='student_view',),
     url(r'^register/$', views.StudentRegister.as_view(),
         name="student_register",),
     url(r'^student/edit/', views.edit_profile, name="student_profile_edit",),
