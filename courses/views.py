f4772f54295ba23b3a5d8c5e192fdf82516e3021
nalorakq8 <yousefmon@gmail.com>
2015-12-02 23:32:32 +0300

    work inprogress

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .forms import NewCourseForm, GradeForm
-from .models import GradeColumn
+
 
 from students.models import Student
-from courses.models import Course
+from courses.models import Course, CourseAnnouncement
 # Create your views here.
 
 
@@ -14,8 +13,8 @@ def course_create(request):
     if request.method == 'POST':
         form = NewCourseForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
-            return redirect()
+            obj = form.save()
+            return redirect("/")
     else:
         form = NewCourseForm()
     return render(
@@ -27,10 +26,16 @@ def course_create(request):
     )
 
 
-class CourseGradeView(ListView):
-    model = GradeColumn
-    template_name = "course_grade.html"
+def list_course_grade_column(request, course_id):
+    course_obj = get_object_or_404(Course, pk=course_id)
+    qs = course_obj.gradecolumn_set.all()
 
+    return render(request, "course_grade.html",
+        {
+            'course': course_obj,
+            'gradecolumns': qs,
+        }
+    )
 
 
 def enroll_student_to_course(request, course_id, student_id):
@@ -56,3 +61,15 @@ def post_student_grade(request):
             "form": form,
         }
     )
+
+def instructor_view_course_stundets_announcments(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    students = Student.objects.filter(course__pk=course_id)
+    announcments = CourseAnnouncement.objects.filter(course__pk=course_id)
+    return render(request, "course_details_announcments.html",
+                  {
+                      'course': course,
+                      'students': students,
+                      'announcments': announcments
+                  }
+                  )
