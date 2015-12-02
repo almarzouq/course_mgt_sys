43919442668428e5547d421a262dbde77d9bfd56
nalorakq8 <yousefmon@gmail.com>
2015-12-02 23:41:48 +0300

    work inprogress

--- courses/urls.py
+++ courses/urls.py
@@ -4,8 +4,6 @@ from courses import views
 urlpatterns = [
     url(r'^course/create/$', views.course_create,
         name='course_create'),
-    url(r'^course/grade/$', views.CourseGradeView.as_view(),
-        name='course_grade'),
     url(r'^course/(?P<course_id>\d+)/enroll/(?P<student_id>\d+)',
         views.enroll_student_to_course, name='enroll'),
     url(r'^course/(?P<course_id>\d+)/post/grade/$', views.post_student_grade,
