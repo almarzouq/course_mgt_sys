0762ba4d73483b30a39190070bbffb61c9f2b3a6
almarzouq <qmis318@gmail.com>
2015-12-05 18:22:07 +0300

    Revert "e_widget"

--- students/urls.py
+++ students/urls.py
@@ -12,12 +12,7 @@ urlpatterns = [
     url(r'^student/edit/(?P<student_id>\d+)$', views.edit_profile, name="student_edit",),
     url(r'^filter/(?P<search_text>\w+)/$',
         views.student_search, name="student_search",),
-
     url(r'^list$', views.students_list, name='students_list',),
     url(r'^list/uni/$', views.StudentListUni.as_view(), name='student_list_order_uni'),
     url(r'^list/name/$', views.StudentListName.as_view(), name='student_list_order_name'),
-
-    url(r'^',views.dateTimeViewBootstrap3 , name='test',),
-
-
 ]
