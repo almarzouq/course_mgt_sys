6b45d6190dce83c9abf161be0f2b149733c24d39
Emperor <nsoulper@gmail.com>
2015-12-10 10:33:16 +0300

    void

--- instructors/forms.py
+++ instructors/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from .models import Instructor, Announcement, Appointment
 from courses.models import GradeColumn
 
+from datetimewidget.widgets import DateTimeWidget, DateWidget, TimeWidget
+
 
 class InstructorForm(forms.ModelForm):
 
@@ -26,12 +28,18 @@ class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = '__all__'
+<<<<<<< 742f614555584919ebab08336345e4f17efeb593
         widgets = {
             'approved': forms.HiddenInput,
             'sent_1st_reminder': forms.HiddenInput,
             'sent_2nd_reminder': forms.HiddenInput,
             'instructor': forms.HiddenInput,
             }
+=======
+        widget = {
+        'date_time' :  DateTimeWidget(attrs={'id':"yourdatetimeid"}, usel10n = True, bootstrap_version=3)
+        }
+>>>>>>> progress
 
 
 class GradeColumnEditForm(forms.ModelForm):
