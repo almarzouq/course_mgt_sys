6e47f7a0302a6f72065a97dee94eeee45aa6fa03
nalorakq8 <yousefmon@gmail.com>
2015-12-17 22:45:49 +0300

    fixing problems

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Student
 
-
-class StudentAdmin (admin. ModelAdmin):
-    list_display = ('name', 'email', 'university_id', "twitter_id")
-
-admin.site.register(Student, StudentAdmin)
+# Register your models here.
