49f03570aac633de00faf7041d65b8d38e28191c
Emperor <nsoulper@gmail.com>
2015-12-19 21:08:56 +0300

    void

--- students/forms.py
+++ students/forms.py
@@ -6,19 +6,25 @@ class StudentEditForm(forms.ModelForm):
 
     class Meta:
         model = Student
-        exclude = ['name', 'university_id']
+        exclude = ['name']
+        widgets = {
+
+        'user' : forms.HiddenInput,
+        }
 
 
-class SignupForm(forms.Form):
-
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
     # university_id = forms.CharField(required=False)
     # twitter_id = forms.CharField(required=False)
-
+    
     def signup(self, request, user):
         Student.objects.create(
             user=user,
             name=user.username,
             email=user.email,
-            # university_id=self.cleaned_data.get('university_id'),
-            # twitter_id=self.cleaned_data.get('twitter_id')
+            university_id=self.cleaned_data.get('university_id'),
+            twitter_id=self.cleaned_data.get('twitter_id')
         )
